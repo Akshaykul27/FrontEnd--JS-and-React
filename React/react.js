b321b9f7e5f1709165fba7445a2c65e476654c81
AkShay Kulkarni <akshaykulkarni8496@gmail.com>
2022-06-27 22:21:37 +0530

    React

---
+++ React/react.js
@@ -0,0 +1,36 @@
+ /* 
+      # React :  
+         - all about  componant
+         - What to do! Not how to do!
+         - SPA - Single Page Application (single html page, rest everything controlled by React.js library)
+         - use  Virtual DOM (blue print of DOM)
+
+
+      # API's (Application Programming Interface):
+         - usning API Frontend And Backend communicate with each other
+         
+      # DB (Data Base):
+         - Entire Report keep storing
+
+      # CDN (Conent Delivary Network):
+         - Type of server
+         - dump server 
+         - client fatching  server
+
+
+      # mpm (Node Pakage Management)
+      # npx (Node Pakage Exicution)
+
+      -create-react-app
+
+      npx create-react-app react-js-1
+      
+*/
+
+/*
+files of projet:
+
+1. HTML file (Empty)(index.html)
+2. Bundle files( n numbers - based on size of project)(bundle.js) (HTML, CSS, JS extracted form client side )
+*/
+ 
